610454618e24c7ef58aaf621ec5d4053e90d2e79
chenx <1797532878@qq.com>
2023-03-19 15:59:04 +0800

    整合dubbo，使用公共service

--- src/main/java/com/api/apiinterface/controller/NameController.java
+++ src/main/java/com/api/apiinterface/controller/NameController.java
@@ -30,27 +30,27 @@ public class NameController {
     @PostMapping("/user")
     public String getUserNameByPost(@RequestBody User user, HttpServletRequest request) throws UnsupportedEncodingException {
 
-        String accessKey = new String(request.getHeader("accessKey").getBytes("ISO-8859-1"),StandardCharsets.UTF_8);
-        String nonce = request.getHeader("nonce");
-        String body = new String(request.getHeader("body").getBytes("ISO-8859-1"),StandardCharsets.UTF_8);
-        String timestamp = request.getHeader("timestamp");
-        String sign = request.getHeader("sign");
-
-
-        // todo 实际需要去数据库中查是否分配给了用户
-        if (!accessKey.equals("asd")) {
-            throw new RuntimeException("无权限！");
-        }
-        if (Long.parseLong(nonce) > 1000) {
-            throw new RuntimeException("无权限！");
-        }
-
-        // todo 数据库查到secretKey
-        String serverSign = SignUtils.genSign(body, "asdasdasd");
-
-        if (!serverSign.equals(sign)) {
-            throw new RuntimeException("无权限！");
-        }
+//        String accessKey = new String(request.getHeader("accessKey").getBytes("ISO-8859-1"),StandardCharsets.UTF_8);
+//        String nonce = request.getHeader("nonce");
+//        String body = new String(request.getHeader("body").getBytes("ISO-8859-1"),StandardCharsets.UTF_8);
+//        String timestamp = request.getHeader("timestamp");
+//        String sign = request.getHeader("sign");
+//
+//
+//        // todo 实际需要去数据库中查是否分配给了用户
+//        if (!accessKey.equals("asd")) {
+//            throw new RuntimeException("无权限！");
+//        }
+//        if (Long.parseLong(nonce) > 1000) {
+//            throw new RuntimeException("无权限！");
+//        }
+//
+//        // todo 数据库查到secretKey
+//        String serverSign = SignUtils.genSign(body, "asdasdasd");
+//
+//        if (!serverSign.equals(sign)) {
+//            throw new RuntimeException("无权限！");
+//        }
 
         // todo 时间不能超过当前时间相邻5分钟
         return "POST YOUR NAME IS " + user.getName();
